42233221100a5269022f5d146fdf493aebe01e08
krishna murari <kmurari@alpha-sense.com>
2020-02-18 16:45:17 +0530

    nth fibonacci matri exponentiation

---
+++ matrix exponentiation/nthfib.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mp(a,b) make_pair(a,b)
+#define p_int pair<int,int>
+using namespace std;
+p_int fib(int n){
+    if(n==0){
+        return mp(1,0);
+    }
+    p_int t = fib(n/2);
+    int x = t.first + t.second;
+    int y = t.first;
+    if(n%2){
+        return mp(x+y,x);
+    }else{
+        return mp(x,y);
+    }
+}
+int main()
+{
+    int n=8;
+    p_int t = fib(n-1);
+    cout << t.first << endl;
+    return 0;
+}
